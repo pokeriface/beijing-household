da89f2f889f168fcdc0b3d67fd24ff5a17aea21a
Jun, Liu <liujun_it@hotmail.com>
2020-10-16 00:20:43 +0800

    [BJ-0003] Add columns name

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # beijing-household
 Analyse the candidates settled in Beijing by bonus regulation each year From 2018 to now.
+
+All data is from public web site.
+The data of 2020 is from `http://fuwu.rsj.beijing.gov.cn/nwesqintegralpublic/settleperson/settlePersonTable`
--- data-mining/mining.py
+++ data-mining/mining.py
@@ -4,7 +4,7 @@ import numpy as np
 from pandas import Series
 from pandas import DataFrame
 
-candidates = pd.read_csv('./data/2020/candidates.txt', sep="\t", header = 0)
+candidates = pd.read_csv('./data/2020/candidates.txt', sep="\t", names = ['id', 'name', 'birth', 'company', 'score'])
 
 print("count:") 
 print(candidates.count())
