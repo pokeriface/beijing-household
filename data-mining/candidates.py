4f470447036a534c0580fb83718b8ad40c0e033e
Jun, Liu <liujun_it@hotmail.com>
2020-10-18 20:33:21 +0800

    [BJ-0004] Add all candidates and year distribution

--- data-mining/candidates.py
+++ data-mining/candidates.py
@@ -10,14 +10,5 @@ def year(yyyyMM: str):
     return yyyyMM[0:4]
 
 candidates = pd.read_csv('data/2020/candidates.txt', sep="\t", names = ['id', 'name', 'birth', 'company', 'score'])
-
-print("count:")
-print(candidates.count())
-print(type(candidates))
-
-print()
-
-maxbirth = candidates[['id', 'birth']]
-maxbirth['birth'] = maxbirth['birth'].map(year)
-dist = maxbirth.groupby(by='birth').count()
-dist.plot(kind='bar',title='Year Distribution')
+candidates['year'] = candidates['birth'].map(year)
+print(candidates[candidates['year'] >= '1985'])
